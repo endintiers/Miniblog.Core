20fe5a942fa746d3a8a073fc7ec5c8f4046d3b57
Chris Hewitt <chris.hewitt@outlook.com.au>
2018-12-01 02:17:44 +1100

    Moving DEckchairs

--- src/BlogSettings.cs
+++ src/BlogSettings.cs
@@ -2,8 +2,8 @@
 {
     public class BlogSettings
     {
-        public string Owner { get; set; } = "The Owner";
+        public string Owner { get; set; } = "Unearth AI";
         public int PostsPerPage { get; set; } = 2;
-        public int CommentsCloseAfterDays { get; set; } = 10;
+        public int CommentsCloseAfterDays { get; set; } = 30;
     }
 }
